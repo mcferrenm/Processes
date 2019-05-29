cc7437a3313950b699d9a55e9fdba60c71b8871c
mcferrenm <mcferrenmax@gmail.com>
2019-05-29 15:52:18 -0400

    complete ex2.c

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,40 @@
 
 int main(void)
 {
-    // Your code here 
+    FILE *fp;
+    char c;
+
+    fp = fopen("text.txt", "r+");
+    
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        fprintf(fp, "%d", 100);
+
+        while(1) {
+            c = fgetc(fp);
+            if ( feof(fp) ) {
+                break;
+            }
+            printf("Child process: %c\n", c);
+        }
+    } else {
+        fprintf(fp, "%d", 50);
+
+         while(1) {
+            c = fgetc(fp);
+            if ( feof(fp) ) {
+                break;
+            }
+            printf("Parent process: %c\n", c);
+        }
+    }
+
+    fclose(fp);
+    
     
     return 0;
 }
